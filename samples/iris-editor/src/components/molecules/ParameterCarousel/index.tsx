fca496e0baa11820739d1c609942af6f74b42682
Jon Mulligan <jonmulligan@me.com>
2021-06-24 04:22:54 +0900

    Storing state as parameter name instead of index. Code is a little dirty for now

--- samples/iris-editor/src/components/molecules/ParameterCarousel/index.tsx
+++ samples/iris-editor/src/components/molecules/ParameterCarousel/index.tsx
@@ -1,32 +1,35 @@
+import { AdjustmentParameter } from "@iris/core";
 import { FC, useMemo } from "react";
 import { Carousel } from "../../atoms/Carousel";
 import { ParameterChip } from "../../atoms/ParameterChip";
 import { ParameterDivider } from "../../atoms/ParameterDivider";
 
+type ParameterChipDef = { icon: string; label: string; index: number; parameter: AdjustmentParameter | "crop" }
+
 const parameters: (
   | "DIVIDER"
-  | { icon: string; label: string; index: number }
+  | ParameterChipDef
 )[] = [
-  { icon: "crop", label: "Crop", index: 0 },
+  { icon: "crop", label: "Crop", index: 0, parameter: "crop" },
   "DIVIDER",
-  { icon: "light_mode", label: "Brightness", index: 1 },
-  { icon: "exposure", label: "Exposure", index: 2 },
-  { icon: "tonality", label: "Contrast", index: 3 },
-  { icon: "hdr_strong", label: "Highlights", index: 4 },
-  { icon: "hdr_weak", label: "Shadows", index: 5 },
+  { icon: "light_mode", label: "Brightness", index: 1, parameter: "brightness" },
+  { icon: "exposure", label: "Exposure", index: 2, parameter: "exposure" },
+  { icon: "tonality", label: "Contrast", index: 3, parameter: "contrast" },
+  { icon: "hdr_strong", label: "Highlights", index: 4, parameter: "highlights" },
+  { icon: "hdr_weak", label: "Shadows", index: 5, parameter: "shadows" },
   "DIVIDER",
-  { icon: "thermostat", label: "Warmth", index: 6 },
-  { icon: "colorize", label: "Tint", index: 7 },
-  { icon: "invert_colors", label: "Saturation", index: 8 },
+  { icon: "thermostat", label: "Warmth", index: 6, parameter: "warmth" },
+  { icon: "colorize", label: "Tint", index: 7, parameter: "tint" },
+  { icon: "invert_colors", label: "Saturation", index: 8, parameter: "saturation" },
   "DIVIDER",
-  { icon: "details", label: "Sharpness", index: 9 },
-  { icon: "grain", label: "Grain", index: 10 },
-  { icon: "vignette", label: "Vignette", index: 11 },
+  { icon: "details", label: "Sharpness", index: 9, parameter:"sharpness" },
+  { icon: "grain", label: "Grain", index: 10, parameter: "grain" },
+  { icon: "vignette", label: "Vignette", index: 11, parameter: "vignette" },
 ];
 
 export type ParameterCarouselProps = {
-  selectedParameter: number | undefined;
-  onClickParameter(index: number): void;
+  selectedParameter: AdjustmentParameter | "crop" | "none";
+  onClickParameter(parameter: AdjustmentParameter | "crop" | "none"): void;
 };
 
 export const ParameterCarousel: FC<ParameterCarouselProps> = (props) => {
@@ -41,8 +44,8 @@ export const ParameterCarousel: FC<ParameterCarouselProps> = (props) => {
       paddingX={hasSelectedItem ? "50%" : "24px"}
       snap={hasSelectedItem ? "center" : "start"}
       snapSkipConstructors={[ParameterDivider]}
-      focusedItem={selectedParameter}
-      onFocusedItemChange={onClickParameter}
+      focusedItem={(parameters.find(p => p !== "DIVIDER" && p.parameter === selectedParameter) as ParameterChipDef | undefined)?.index || 0}
+      onFocusedItemChange={(index) => onClickParameter((parameters.find(p => p !== "DIVIDER" && p.index === index) as ParameterChipDef | undefined)?.parameter || "none")}
     >
       {parameters.map((param, index) =>
         param === "DIVIDER" ? (
@@ -53,13 +56,13 @@ export const ParameterCarousel: FC<ParameterCarouselProps> = (props) => {
             variant={
               selectedParameter === undefined
                 ? "default"
-                : selectedParameter === param.index
+                : selectedParameter === param.parameter
                 ? "active"
                 : "inactive"
             }
             icon={param.icon}
             label={param.label}
-            onClick={() => onClickParameter(param.index)}
+            onClick={() => onClickParameter(param.parameter)}
           />
         )
       )}
--- samples/iris-editor/src/components/organisms/EditorControls/index.tsx
+++ samples/iris-editor/src/components/organisms/EditorControls/index.tsx
@@ -5,13 +5,26 @@ import { useAdjustment } from "@iris/react";
 import { ParameterControl } from "../../atoms/ParameterControl";
 import { ModeSelector } from "../../molecules/ModeSelector";
 import { ParameterCarousel } from "../../molecules/ParameterCarousel";
+import { AdjustmentParameter } from "@iris/core";
 
 export type EditorControlsProps = HTMLAttributes<HTMLElement>;
 
+const AdjustmentControl: FC<{parameter: AdjustmentParameter}> = (props) => {
+  const [paramValue, setParamValue] = useAdjustment(props.parameter);
+
+  return (
+    <ParameterControl
+      value={paramValue}
+      onChange={setParamValue}
+      min={-1}
+      max={1}
+    />
+  )
+}
+
 export const EditorControls: FC<EditorControlsProps> = (baseProps) => {
   const [selectedParameter, setSelectedParameter] =
-    useState<number | undefined>(undefined);
-  const [paramValue, setParamValue] = useAdjustment("brightness");
+    useState<AdjustmentParameter | "crop" | "none">("none");
 
   return (
     <section
@@ -25,13 +38,11 @@ export const EditorControls: FC<EditorControlsProps> = (baseProps) => {
         selectedParameter={selectedParameter}
         onClickParameter={setSelectedParameter}
       />
-      <ParameterControl
-        value={paramValue}
-        onChange={setParamValue}
-        min={-1}
-        max={1}
-      />
-      {/* <ModeSelector /> */}
+      {selectedParameter === "none" || selectedParameter === "crop" ? (
+        <ModeSelector />
+      ) : (
+        <AdjustmentControl parameter={selectedParameter} />
+      )}
     </section>
   );
 };
