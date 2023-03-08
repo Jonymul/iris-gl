5e99777a16c89abc7a7a03ea706344e23f0825be
Jon Mulligan <jonymulligan@gmail.com>
2023-03-08 18:51:04 +0900

    Splitting shaders into separate files for readability

--- packages/core/src/CanvasRenderer/index.ts
+++ packages/core/src/CanvasRenderer/index.ts
@@ -9,94 +9,8 @@ import {
   warnUnknownName,
   warnUnknownType,
 } from "./warnings";
-
-const BASE_VERTEX_SHADER = `
-  attribute vec2 position;
-  varying vec2 texCoords;
-
-  void main() {
-    texCoords = (position + 1.0) / 2.0;
-    texCoords.y = 1.0 - texCoords.y;
-    gl_Position = vec4(position, 0, 1.0);
-  }
-`;
-
-const BASE_FRAGMENT_SHADER = `
-  precision highp float;
-  varying vec2 texCoords;
-
-  uniform sampler2D textureSampler;
-  uniform float brightness;
-  uniform float exposure;
-  uniform float contrast;
-  uniform float shadows;
-  uniform float highlights;
-  uniform float saturation;
-  uniform float warmth;
-  uniform float tint;
-
-  vec3 adjustBrightness(vec3 color, float brightness) {
-    return color + brightness;
-  }
-
-  vec3 adjustExposure(vec3 color, float exposure) {
-    return color * pow(2.0, exposure);
-  }
-
-  vec3 adjustContrast(vec3 color, float contrast) {
-    return 0.5 + (contrast + 1.0) * (color.rgb - 0.5);
-  }
-
-  vec3 adjustSaturation(vec3 color, float saturation) {
-    // WCAG 2.1 relative luminance base
-    const vec3 luminanceWeighting = vec3(0.2126, 0.7152, 0.0722);
-    vec3 grayscaleColor = vec3(dot(color, luminanceWeighting));
-    return mix(grayscaleColor, color, 1.0 + saturation);
-  }
-
-  vec3 adjustTempTint(vec3 color, float warmth, float tint) {
-    const vec3 warmFilter = vec3(0.93, 0.54, 0.0);
-    const mat3 RGBtoYIQ = mat3(0.299, 0.587, 0.114, 0.596, -0.274, -0.322, 0.212, -0.523, 0.311);
-    const mat3 YIQtoRGB = mat3(1.0, 0.956, 0.621, 1.0, -0.272, -0.647, 1.0, -1.105, 1.702);
-
-    // adjusting tint
-    vec3 yiq = RGBtoYIQ * color;
-    yiq.b = clamp(yiq.b + tint*0.5226*0.1, -0.5226, 0.5226);
-    vec3 rgb = YIQtoRGB * yiq;
-
-    // adjusting warmth
-    vec3 processed = vec3(
-      (rgb.r < 0.5 ? (2.0 * rgb.r * warmFilter.r) : (1.0 - 2.0 * (1.0 - rgb.r) * (1.0 - warmFilter.r))),
-      (rgb.g < 0.5 ? (2.0 * rgb.g * warmFilter.g) : (1.0 - 2.0 * (1.0 - rgb.g) * (1.0 - warmFilter.g))),
-      (rgb.b < 0.5 ? (2.0 * rgb.b * warmFilter.b) : (1.0 - 2.0 * (1.0 - rgb.b) * (1.0 - warmFilter.b)))
-    );
-    return mix(rgb, processed, warmth);
-  }
-
-  vec3 adjustShadowsHighlights(vec3 color, float shadows, float highlights) {
-    const vec3 luminanceWeighting = vec3(0.3, 0.3, 0.3);
-    mediump float luminance = dot(color, luminanceWeighting);
-
-    mediump float shadow = clamp((pow(luminance, 1.0/(shadows+1.0)) + (-0.76)*pow(luminance, 2.0/(shadows+1.0))) - luminance, 0.0, 1.0);
-    mediump float highlight = clamp((1.0 - (pow(1.0-luminance, 1.0/(1.0-highlights)) + (-0.8)*pow(1.0-luminance, 2.0/(1.0-highlights)))) - luminance, -1.0, 0.0);
-    lowp vec3 result = vec3(0.0, 0.0, 0.0) + ((luminance + shadow + highlight) - 0.0) * ((color - vec3(0.0, 0.0, 0.0))/(luminance - 0.0));
-
-    return result;
-  }
-
-  void main() {
-    vec4 color = texture2D(textureSampler, texCoords);
-    
-    color.rgb = adjustShadowsHighlights(color.rgb, shadows, highlights);
-    color.rgb = adjustExposure(color.rgb, exposure);
-    color.rgb = adjustBrightness(color.rgb, brightness);
-    color.rgb = adjustContrast(color.rgb, contrast);
-    color.rgb = adjustTempTint(color.rgb, warmth, tint);
-    color.rgb = adjustSaturation(color.rgb, saturation);
-
-    gl_FragColor = color;
-  }
-`;
+import { shaderSrcVertex } from "./shaders/vertex";
+import { shaderSrcFragment } from "./shaders/fragment";
 
 const VERTICES = new Float32Array([-1, -1, -1, 1, 1, 1, -1, -1, 1, 1, 1, -1]);
 
@@ -130,8 +44,8 @@ export class CanvasRenderer {
 
     this.clearCanvas();
     const program = this.createProgram([
-      this.shaderCompiler.compileVertexShader(BASE_VERTEX_SHADER),
-      this.shaderCompiler.compileFragmentShader(BASE_FRAGMENT_SHADER),
+      this.shaderCompiler.compileVertexShader(shaderSrcVertex),
+      this.shaderCompiler.compileFragmentShader(shaderSrcFragment),
     ]);
     this.useProgram(program);
     this.getUniforms();
---
+++ packages/core/src/CanvasRenderer/shaders/vertex.ts
@@ -0,0 +1,12 @@
+const glsl = (x: unknown): string => x as string;
+
+export const shaderSrcVertex = glsl`
+  attribute vec2 position;
+  varying vec2 texCoords;
+
+  void main() {
+    texCoords = (position + 1.0) / 2.0;
+    texCoords.y = 1.0 - texCoords.y;
+    gl_Position = vec4(position, 0, 1.0);
+  }
+`;
